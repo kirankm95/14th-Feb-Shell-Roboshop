39506df661c8125c709fd8ad061973e6ffea5e1f
Kiran <kirankm95@gmail.com>
2026-02-26 11:47:01 +0530

    new ansible playbook for roboshop

---
+++ ec2-instances-route53.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+AMI="ami-0b4f379183e5706b9"
+SG="sg-0ec1d694d0be0920e"
+SUBNET="subnet-0b81d24fb28353d3f"
+HOSTED_ZONE="Z09372402SX8LY0VINKFB"
+DOMAIN="kiranku.online"
+
+APP=("web" "catalogue" "cart" "user" "shipping")
+
+for i in "${APP[@]}"
+do
+  echo "Creating instance for $i"
+
+  INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id "$AMI" \
+    --instance-type t3.micro \
+    --security-group-ids "$SG" \
+    --subnet-id "$SUBNET" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${i}}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text)
+
+  echo "Instance ID: $INSTANCE_ID"
+
+  aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+
+  PRIVATE_IP=$(aws ec2 describe-instances \
+      --instance-ids "$INSTANCE_ID" \
+      --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+      --output text)
+
+  echo "Private IP: $PRIVATE_IP"
+
+  JSON_FILE=$(mktemp)
+  cat > "$JSON_FILE" <<EOF
+{
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "${i}.${DOMAIN}",
+        "Type": "A",
+        "TTL": 300,
+        "ResourceRecords": [
+          { "Value": "${PRIVATE_IP}" }
+        ]
+      }
+    }
+  ]
+}
+EOF
+
+  aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE" --change-batch "file://$JSON_FILE"
+
+  rm -f "$JSON_FILE"
+
+  echo "Route53 record created for ${i}.${DOMAIN}"
+  echo "----------------------------------------"
+done
